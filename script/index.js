b9ef81c58aa351507b9f22ae7dac6789b9a6c325
mighty <mighty2815736@gmail.com>
2022-05-03 13:16:44 +0300

    Image platforms

--- script/index.js
+++ script/index.js
@@ -3,8 +3,8 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = 1024;
+canvas.height = 576;
 
 const gravity = 1.5;
 
@@ -39,26 +39,29 @@ class Player {
 }
 
 class Platform {
-  constructor({x, y}) {
+  constructor({x, y, image}) {
     this.position = {
       x,
       y
     }
-    this.width = 200;
-    this.height = 20;
+    this.image = image;
+    this.width = image.width;
+    this.height = image.height;
   }
 
   draw() {
-    c.fillStyle = 'blue';
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    c.drawImage(this.image, this.position.x, this.position.y)
   }
 }
 
+const image = new Image()
+image.src = './sprites/platform.png'
+
 const player = new Player();
 const platforms = [new Platform({
-  x: 200, y: 100
+  x: -1, y: 470, image
 }), new Platform({
-  x: 500, y: 200})];
+  x: image.width - 3, y: 470, image})];
 
 const keys = {
   right: {
@@ -73,11 +76,12 @@ let scrollOffset = 0
 
 function animate() {
   requestAnimationFrame(animate);
-  c.clearRect(0, 0, canvas.width, canvas.height);
-  player.update();
+  c.fillStyle = 'white';
+  c.fillRect(0, 0, canvas.width, canvas.height);
   platforms.forEach(platform => {
     platform.draw();
   })
+  player.update()
 
   if (keys.right.pressed && player.position.x < 400) {
     player.velocity.x = 5
@@ -130,7 +134,7 @@ window.addEventListener('keydown', ({keyCode}) => {
 
     case 87:
       console.log('up');
-      player.velocity.y -= 20;
+      player.velocity.y -= 25;
       break;
   }
 })

8160f77c742cc28bf73d44b24f0c3d67f693fe11
mighty <mighty2815736@gmail.com>
2022-05-04 15:40:40 +0300

    little tuning with left scroll

--- script/index.js
+++ script/index.js
@@ -175,7 +175,7 @@ function animate() {
 
   if (keys.right.pressed && player.position.x < 400) {
     player.velocity.x = player.speed
-  } else if (keys.left.pressed && player.position.x > 100) {
+  } else if ((keys.left.pressed && player.position.x > 100) || (keys.left.pressed && scrollOffset === 0 && player.position.x > 0)) {
     player.velocity.x = -player.speed
   } else {
     player.velocity.x = 0
@@ -188,7 +188,7 @@ function animate() {
       genericObjects.forEach(GenericObject => {
         GenericObject.position.x -= player.speed * .66
       })
-    } else if (keys.left.pressed) {
+    } else if (keys.left.pressed && scrollOffset > 0) {
       scrollOffset -= player.speed
       platforms.forEach(platform => {
         platform.position.x += player.speed
